d795bffc6549f16f8bc48cdadb1cac6dda426294
Suraj Kumar <74973973+1Suraj-Kumar@users.noreply.github.com>
2024-03-27 07:49:29 +0000

    This is comparison

---
+++ basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);   //doing comparison of diffrent data types is not a good way , sometimes it might be return diffrent result
+console.log("02" > 1);
+console.log("02" == 1);
+
+// strict check ===
+console.log("2" === 1); //it will do strict check not convert data type 
